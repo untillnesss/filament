df884a99c344e754dda97f31bde27643441ad5b9
Dan Harrin <git@danharrin.com>
2024-12-10 12:50:46 +0000

    Update livewire.blade.php

--- packages/schemas/resources/views/components/livewire.blade.php
+++ packages/schemas/resources/views/components/livewire.blade.php
@@ -1,19 +1,22 @@
 @if (filled($id = $getId()) || filled($extraAttributes = $getExtraAttributes()))
-    <div
-        {{
-            $attributes
-                ->merge([
-                    'id' => $getId(),
-                ], escape: false)
-                ->merge($getExtraAttributes(), escape: false)
-        }}
+    {!! '<div' !!}
+    {{-- Avoid formatting issues with unclosed elements --}}
+    {{
+        $attributes
+            ->merge([
+                'id' => $getId(),
+            ], escape: false)
+            ->merge($getExtraAttributes(), escape: false)
+    }}
     >
 @endif
+
 @if (filled($key = $getLivewireKey()))
     @livewire($getComponent(), $getComponentProperties(), key($key))
 @else
     @livewire($getComponent(), $getComponentProperties())
 @endif
 @if (filled($id) || filled($extraAttributes))
-    </div>
+    {!! '</div>' !!}
+    {{-- Avoid formatting issues with unclosed elements --}}
 @endif
