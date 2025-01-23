f3162765a18fa24cd1d17a517dd93a1b415149b1
Dan Harrin <git@danharrin.com>
2025-01-23 23:02:05 +0000

    Use oklch colors

--- packages/support/src/Colors/ColorManager.php
+++ packages/support/src/Colors/ColorManager.php
@@ -41,35 +41,15 @@ class ColorManager
 
     /**
      * @param  array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string  $color
-     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | string
+     * @return array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string}
      */
-    public function processColor(array | string $color): array | string
+    public function processColor(array | string $color): array
     {
-        if (is_string($color) && str_starts_with($color, '#')) {
-            return Color::hex($color);
+        if (is_string($color)) {
+            return Color::palette($color);
         }
 
-        if (is_string($color) && str_starts_with($color, 'rgb')) {
-            return Color::rgb($color);
-        }
-
-        if (is_array($color)) {
-            return array_map(function (string $color): string {
-                if (str_starts_with($color, 'oklch')) {
-                    return $color;
-                }
-
-                if (str_starts_with($color, '#') || str_starts_with($color, 'rgb')) {
-                    throw new \Exception('Invalid color format.');
-                    // convert $color
-                }
-
-                throw new \Exception('Invalid color format.');
-                // convert "rgb({$color})"
-            }, $color);
-        }
-
-        return $color;
+        return array_map(Color::convertToOklch(...), $color);
     }
 
     /**
