b491a15e1c747079aa57881fbf5ec04374846a3e
Dan Harrin <git@danharrin.com>
2025-01-30 08:24:27 +0000

    Link color

--- packages/support/src/View/Components/Link.php
+++ packages/support/src/View/Components/Link.php
@@ -2,6 +2,8 @@
 
 namespace Filament\Support\View\Components;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Filament\Support\View\Components\Contracts\HasColor;
 use Filament\Support\View\Components\Contracts\HasDefaultGrayColor;
 
@@ -13,6 +15,43 @@ class Link implements HasColor, HasDefaultGrayColor
      */
     public function getColorClasses(array $color): array
     {
-        return [];
+        $gray = FilamentColor::getColor('gray');
+
+        ksort($color);
+
+        $darkestLightGrayBg = $gray[50];
+
+        foreach ($color as $shade => $shadeValue) {
+            if (Color::isTextContrastRatioAccessible($darkestLightGrayBg, $shadeValue)) {
+                $text = $shade;
+
+                break;
+            }
+        }
+
+        $text ??= 900;
+
+        krsort($color);
+
+        $lightestDarkGrayBg = $gray[700];
+
+        foreach ($color as $shade => $shadeValue) {
+            if ($shade > 400) {
+                continue;
+            }
+
+            if (Color::isTextContrastRatioAccessible($lightestDarkGrayBg, $shadeValue)) {
+                $darkText = $shade;
+
+                break;
+            }
+        }
+
+        $darkText ??= 200;
+
+        return [
+            "fi-text-color-{$text}",
+            "dark:fi-text-color-{$darkText}",
+        ];
     }
 }
